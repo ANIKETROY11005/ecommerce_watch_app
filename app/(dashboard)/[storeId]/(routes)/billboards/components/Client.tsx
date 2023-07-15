b5bf0c2402e4cf7ec426fcd74b4130022f6f2198
ANIKETROY11005 <draxnme12345@gmail.com>
2023-07-15 14:35:55 +0530

    build: added components

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/Client.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { Heading } from '@/components/ui/Heading';
+import { ApiList } from '@/components/ui/api-list';
+import { Button } from '@/components/ui/button';
+import { DataTable } from '@/components/ui/data-table';
+import { Separator } from '@/components/ui/separator';
+import { Plus } from 'lucide-react';
+import { useParams, useRouter } from 'next/navigation';
+import { BillboardColumn, columns } from './columns';
+
+interface BillboardClientProps {
+  data: BillboardColumn[];
+}
+
+const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Billboards (${data.length})`}
+          description="Manage billboards for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="label" />
+      <Heading title="API" description="API calls for billboards" />
+      <Separator />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
+    </>
+  );
+};
+
+export default BillboardClient;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 
 import { Inter } from 'next/font/google';
 
+import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -21,6 +22,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <Toaster />
           <ModalProvider />
           {children}
         </body>
